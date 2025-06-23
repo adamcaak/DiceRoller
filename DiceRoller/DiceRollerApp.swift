eb266649d6f3e4683631c199f6bc4c165fb55d34
adamcaak <ladamczak1997@gmail.com>
2025-06-23 23:39:01 +0200

    Initial Commit

---
+++ DiceRoller/DiceRollerApp.swift
@@ -0,0 +1,17 @@
+//
+//  DiceRollerApp.swift
+//  DiceRoller
+//
+//  Created by Łukasz Adamczak on 23/06/2025.
+//
+
+import SwiftUI
+
+@main
+struct DiceRollerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
