5df478235034b6f3a7391f5aa09f8b9608b50e87
adamcaak <ladamczak1997@gmail.com>
2025-06-24 00:03:29 +0200

    Update max dice on display

--- DiceRoller/ContentView.swift
+++ DiceRoller/ContentView.swift
@@ -33,7 +33,7 @@ struct ContentView: View {
                         numberOfDice += 1
                     }
                 }
-                .disabled(numberOfDice == 3)
+                .disabled(numberOfDice == 5)
                 .buttonStyle(.bordered)
             }
         }
